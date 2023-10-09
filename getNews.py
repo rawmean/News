0b263f74507635990e1ae1c3b56b3ba95ebbb9b6
ramin on Sandy <lesswire1@gmail.com>
2023-10-09 08:58:15 -0700

    update1696867095.094273

--- getNews.py
+++ getNews.py
@@ -45,11 +45,11 @@ def refresh_news():
     system_pre = "you are a helpful assistant"
     question_pre  = f'''
     First separate the following english text into paragraphs based on the topic. 
-    Summarize each paragraph into at most 6 sentences.
     The output must be in pure json and nothing else. 
     Do not include and paragraph that is about daily quiz.
     Do not include any paragraph that is not news.
     Limit hte number of paragraphs to 5. There shall be at most 5 paragraphs.
+    Each paragraph must be converted to simple English.
 
     ### ENGLISH TEXT:
     {article}
